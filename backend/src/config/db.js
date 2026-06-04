8d1dd8c470004c121cbebff1e7cd29c360e44c26
Niral1881 <niralkachchhi1881@gmail.com>
2026-06-04 12:53:54 +0530

    fix mongodb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,15 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected");
-
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-
-    console.log(error);
-
+    console.error(error);
     process.exit(1);
   }
 };
